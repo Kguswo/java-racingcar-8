f8caf59e1905efc26aa3e691ad1af99caa8a00de
Now <khj990322@gmail.com>
2025-10-26 20:12:49 +0900

    test: ApplicationTest 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -27,6 +28,69 @@ class ApplicationTest extends NsTest {
     void 예외_테스트() {
         assertSimpleTest(() ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("예외 테스트: 5글자 초과 이름")
+    void longName_ExceptionTest() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("예외 테스트: 빈 이름 입력")
+    void emptyName_ExceptionTest() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,,woni", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("예외 테스트: 공백만 있는 이름")
+    void blankName_ExceptionTest() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi, ,woni", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("예외 테스트: 중복된 이름")
+    void duplicateName_ExceptionTest() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,pobi", "1"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("예외 테스트: 숫자가 아닌 시도 횟수")
+    void nonNumericRoundCount_ExceptionTest() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,woni", "abc"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("예외 테스트: 0 이하의 시도 횟수")
+    void zeroRoundCount_ExceptionTest() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,woni", "0"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("예외 테스트: 음수 시도 횟수")
+    void negativeRoundCount_ExceptionTest() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("pobi,woni", "-1"))
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
